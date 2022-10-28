f95fba3e0aee61fe62872c9eb0064f0f36e876d2
Lota Schwager <101421100+LotaSchwager@users.noreply.github.com>
2022-10-28 12:12:40 -0300

    otro añadido

---
+++ core/src/com/mygdx/game/NaveJefe.java
@@ -0,0 +1,63 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+
+public class NaveJefe extends NaveEnemiga{
+
+	public NaveJefe(int vida, Rectangle hitbox, Sound elim, Texture spr, Sound death, Sound hit, int kill, Sound idle) {
+		super(vida, hitbox, elim, spr, death, hit, kill, idle);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void pintarEntidad() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void destruirEntidad() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void disparo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void disparo(Input entrada) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void movimiento() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void movimiento(Input entrada) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void movIzq() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void movDer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
